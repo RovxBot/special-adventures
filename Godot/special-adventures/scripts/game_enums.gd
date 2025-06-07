a82a242a757d7c91598ab0d35353ebf2c4379adb
rovxbot <sam@cooked.beer>
2025-06-07 12:30:12 +0930

    Removing all old content from the Godot project to prepare for a full refactor.

--- Godot/special-adventures/scripts/game_enums.gd
+++
@@ -1,7 +0,0 @@
-class_name GameEnumsClass
-extends Node
-
-# Define the enum at script level since this is an autoload
-enum AttackType {MELEE, RANGED, MAGIC}
-
-# Can contain other enums or utility functions as needed
